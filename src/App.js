60902d2f77d8b337e63419a1958e5974e1635243
jpachecoz <jepach@gmail.com>
2022-06-12 06:29:40 -0500

    Add Header for next features of navigation

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import styled from "@emotion/styled";
 import { getBooks, getMovies, getCharacters } from "./services/GetData";
+import { AppBar, Toolbar } from "@mui/material";
+import { createTheme, ThemeProvider } from "@mui/material/styles"
 import Row from "./components/Row";
 
 const Container = styled.div`
@@ -11,6 +13,18 @@ const Container = styled.div`
   background-position: center;
   background-repeat: no-repeat;
 `
+
+const StyledImage = styled.img`
+  width: 15rem;
+`
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#000000"
+    }
+  }
+})
 
 function App() {
   const [books, setBooks] = useState({});
@@ -44,6 +58,13 @@ function App() {
 
   return (
     <Container>
+      <ThemeProvider theme={theme}>
+        <AppBar position= "sticky" color="primary">
+          <Toolbar>
+            <StyledImage src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b0/Tloftr-logo.svg/1280px-Tloftr-logo.svg.png" alt="Logo"/>
+          </Toolbar>
+        </AppBar>
+      </ThemeProvider>
       {(books.name === undefined || movies.name === undefined || characters.name === undefined) ? "" : (
       <>
         <Row key = {books.name} title = {books.name} data={books.data}/>
